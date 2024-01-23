72778cafc3548636edeba1e00bf123be7197c880
agserrano3 <toni@codegen.sh>
2024-01-23 12:01:38 +0100

    .

---
+++ fibonacci.py
@@ -0,0 +1,15 @@
+def fib(n):
+    """
+    Return the Fibonacci sequence up to n
+    """
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        sequence = [0, 1]
+        while len(sequence) < n:
+            sequence.append(sequence[-1] + sequence[-2])
+        return sequence
---
+++ test_fibonacci.py
@@ -0,0 +1,14 @@
+import unittest
+from fibonacci import fib
+
+class TestFib(unittest.TestCase):
+
+    def test_fib(self):
+        """
+        Test fibonacci sequence
+        """
+        result = fib(5)
+        self.assertEqual(result, [0, 1, 1, 2, 3])
+
+if __name__ == '__main__':
+    unittest.main()
